16a91366f324bead2942f6bc1d267a16f6059f02
rishabhag18 <71484128+rishabhag18@users.noreply.github.com>
2024-02-01 09:56:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var typed = new Typed(".typing",{
+ strings:["Computer Application Graduate", "Web Developer","Software Developer" ," Oasis Intern"],
+ typeSpeed:100,
+ Backspeed:60,
+ loop:true
+})  
+
+
+
+
+
+
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll("li"),
+totalNavList = navList.length;
+for(let i=0; i<totalNavList; i++){
+    const a= navList[i].querySelector("a");
+   a.addEventListener("click",function(){
+    for(let j=0; j<totalNavList; j++){
+        navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active")
+   })
+}
